32f48006bf5a858755b3411dfe58e9f5f542467c
RoneshSingh <78497324+RoneshSingh@users.noreply.github.com>
2023-07-24 15:26:38 -0400

    thymeleaf

--- bookstore/src/main/java/com/example/takeo/bookstore/controller/UserController.java
+++ bookstore/src/main/java/com/example/takeo/bookstore/controller/UserController.java
@@ -1,17 +1,15 @@
 package com.example.takeo.bookstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.takeo.bookstore.model.User;
 import com.example.takeo.bookstore.service.UserService;
 
-@RestController
-@RequestMapping
+@Controller
 public class UserController {
     private UserService userService;
 
